c098af80e78a53134b1d8f1ed8fcc9f4986c6f58
Uzodimma <Charltonujic@rocketmail.com>
2020-12-16 16:46:57 +0100

    Start day_02 challenge

---
+++ day_02_free_fall.py
@@ -0,0 +1,16 @@
+'''Compute the speed of an object when it hits the ground after being dropped
+(To do this, you need to import squareroot from math library)'''
+#your code goes here
+from math import sqrt
+'''Define a constant for the acceleration due to gravity in m/s**2
+(For example: Gravity is 9.8)'''
+#your code goes here
+'''Read the height from which the object is dropped (from the user)'''
+#your code goes here
+'''Compute the final velocity'''
+#your code goes here
+''' Display the result'''
+#your code goes here
+
+k="x\u00b2"
+print("hello"+k)
