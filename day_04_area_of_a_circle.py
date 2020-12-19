c07f12464ea2ab3609b280e3d4cd69c1dc69ae88
Uzodimma <Charltonujic@rocketmail.com>
2020-12-19 15:12:50 +0100

    Correct the unit for volume of a sphere

--- day_04_area_of_a_circle.py
+++ day_04_area_of_a_circle.py
@@ -1,11 +1,11 @@
-''' This program reads the value of radius from a user
+'''This program reads the value of radius from a user
 and uses it to compute the area of a circle and the
 volume of a sphere'''
 
 # Import pi from math module
 from math import pi
 
-# Define the SI unit for area
+# Define the SI unit for area a circle
 meter_square = "m\u00b2"
 
 # Request the value of r from the user
@@ -19,6 +19,9 @@ print("The area of the circle is {0:.2f}{1}".format(area, meter_square))
 
 # Calculate the volume of a sphere
 volume = (4/3)*pi*(r**3)
+
+# Define the SI unit for volume of a sphere
+meter_square = "m\u00b3"
 
 # Print the volume of a sphere
 print("The volume of the sphere is {0:.2f}{1}".format(volume, meter_square))
